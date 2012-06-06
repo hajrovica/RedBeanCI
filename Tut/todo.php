93fbe70f5bf799e64d96b05440ea88d12417e870
Amar H <hajrovica@gmail.com>
2012-06-06 13:04:36 +0200

    start tutorial - categories added

--- Tut/todo.php
+++ Tut/todo.php
@@ -8,4 +8,39 @@ $template = new StampTE(file_get_contents('template.html'));
 
 list($listItem, $pOpt, $cOpt) = $template->collect('listItem|person|category');
 
+$task = R::load('task',  $id);
+
+if (isset($_POST['add'])&& !empty($_POST['task'])) {
+    $task = R::dispense('task');
+    $task->description=$_POST['task'];
+        if (isset($_POST['cats'])) {
+            $task->sharedCategory = R::batch('category', $_POST['cats']);
+        }
+
+    R::store($task);
+
+    }
+
+if (isset($_POST['trash']) && isset($_POST['done'])) {
+    R::trashAll(R::batch('task', $_POST['done']));
+}
+
+foreach (R::find('task') as $t) {
+   $tags = array();
+   foreach ($t->sharedCategory as $c) {
+       $tags[]=$c->label;
+   }
+
+   $template->glue('listItems',  $listItem->copy()
+            ->injectAll(array('description'=>$t->description,
+                              'value'=>$t->id,
+                              'tags'=>implode(',',$tags)))
+    );
+}
+
+foreach (R::find('category') as $c) {
+   $o = $cOpt->copy()->injectAll(array('value'=>$c->id,  'label'=>$c->label));
+   $template->glue('categories', $o);
+}
+
 echo $template;
